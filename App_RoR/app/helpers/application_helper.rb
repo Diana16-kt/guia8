23d514fee0fb631d3290769c0f79a5aff0c0d6f1
Diana16-kt <benavidesdn16@gmail.com>
2025-10-21 20:53:31 -0600

    tuve un error con la carpeta app-ror , donde se puso como submoduloo y se tuvo que eliminar con commit ingresados anterior mente

---
+++ App_RoR/app/helpers/application_helper.rb
@@ -0,0 +1,22 @@
+#module ApplicationHelper
+ #   def full_title(page_title = '')
+  #  base_title = "Rails_App"
+   # if page_title.empty?
+    #base_title
+    #else
+    #page_title + " | " + base_title
+    #end
+#end
+#end
+
+module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Rails_App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+end
+
